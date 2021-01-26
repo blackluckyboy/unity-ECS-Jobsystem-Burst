f9f65f0a7b4c358508da758b77750b4b76159d88
blackluckyboy <45261941+blackluckyboy@users.noreply.github.com>
2021-01-26 10:16:47 +0800

    Create ExportAssetBundles.cs

---
+++ Assets/Editor/ExportAssetBundles.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public class ExportAssetBundles : MonoBehaviour
+    {
+        [@MenuItem("ExportTools/Build Asset Bundles _%#_a")]
+        static void BuildAssetBundles()
+        {
+            //Debug.Log("AssetBundles导出开始");
+            string dir = Path.Combine(Application.streamingAssetsPath,"AssetBundles") ;
+            if(Directory.Exists(dir) == false)
+            {
+                Directory.CreateDirectory(dir); 
+            }
+            BuildPipeline.BuildAssetBundles(dir, BuildAssetBundleOptions.UncompressedAssetBundle, BuildTarget.iOS);
+        }
+    }
+}
